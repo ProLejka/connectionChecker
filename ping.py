721a7505c5ea5d5ba6233da5276af387166fb8e8
Pro Lejka <tomas.misko87@gmail.com>
2022-10-03 07:32:22 +0200

    randomize ping

--- ping.py
+++ ping.py
@@ -2,6 +2,7 @@
 
 from pingutils import PingUtils
 import time
+import random
 
 pinger = PingUtils()
 sitesArray = pinger.get_all_sites()
@@ -15,4 +16,4 @@ while pinger.running:
     for site in sitesArray:
         if not pinger.ping_host(site[1]):
             pinger.insert_stat(site[0])
-    time.sleep(10)
+    time.sleep(10 + random.randrange(0, 6))
